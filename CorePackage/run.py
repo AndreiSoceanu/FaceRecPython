e4a7d9b7db24e5af6d27218e770bbb50a034de1a
Andrei Mircea Soceanu <masoceanu@gmail.com>
2020-04-08 00:37:10 +0300

    Added face encodings functionality, next should be to save the result somewhere

--- CorePackage/run.py
+++ CorePackage/run.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import dlib
 import numpy as np
@@ -6,7 +8,7 @@ video_capture = cv2.VideoCapture(0)  # 1 or 2 for usb 0 for default
 video_capture.set(cv2.CAP_PROP_FPS, 60)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("../ConfigData/shape_predictor_68_face_landmarks.dat")
-dlib.face_recognition_model_v1("../ConfigData/dlib_face_recognition_resnet_model_v1.dat")
+face_recognition_model = dlib.face_recognition_model_v1("../ConfigData/dlib_face_recognition_resnet_model_v1.dat")
 
 # This is the tolerance for face comparisons
 # The lower the number - the stricter the comparison
@@ -15,6 +17,51 @@ dlib.face_recognition_model_v1("../ConfigData/dlib_face_recognition_resnet_model
 # 0.5-0.6 works well
 TOLERANCE = 0.6
 
+origin = open("../ConfigData/persons.data", "r")
+lines = origin.readlines()
+n = int(lines[0])
+lines.pop(0)
+fileNames = []
+keys = []
+encodings = []
+
+
+# This function will take an image and return its face encodings using the neural network
+def get_face_encodings(img):
+    # Detect faces using the face detector
+    detected_faces = detector(img, 1)
+
+    # Get pose/landmarks of those faces
+    # Will be used as an input to the function that computes face encodings
+    # This allows the neural network to be able to produce similar numbers for faces of the same people,
+    # regardless of camera angle and/or face positioning in the image
+    shapes_faces = [predictor(img, face) for face in detected_faces]
+    # For every face detected, compute the face encodings
+    return [np.array(face_recognition_model.compute_face_descriptor(img, face_pose, 1)) for face_pose in shapes_faces]
+
+
+# This function takes a list of known faces
+def compare_face_encodings(known_faces, face):
+    # Finds the difference between each known face and the given face (that we are comparing)
+    # Calculate norm for the differences with each known face
+    # Return an array with True/Face values based on whether or not a known face matched with the given face
+    # A match occurs when the (norm) difference between a known face and the given face is less than or equal to the
+    # TOLERANCE value
+    return np.linalg.norm(known_faces - face, axis=1) <= TOLERANCE
+
+
+for line in lines:
+    name = line.replace('\n', '')
+
+    files = os.listdir("../TrainBase/" + name)
+    fileNames.extend(files)
+    size = len(files)
+    for i in range(0, size):
+        keys.append(name)
+        img = cv2.imread("../TrainBase/" + name + "/" + files[i])
+        encodings.append(get_face_encodings(img))
+# TODO filewrite encodings
+print((encodings))
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
